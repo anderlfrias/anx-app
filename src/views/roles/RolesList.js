8a451960ad8e5608cff4063e1dcb807e16fcb755
Anderson Frias <anderl.friaas@gmail.com>
2024-04-11 16:17:13 -0400

    [Add] Lista de roles

--- src/views/roles/RolesList.js
+++ src/views/roles/RolesList.js
@@ -1,21 +1,66 @@
-import { useEffect } from "react"
+import { Button, Card, Table, Tooltip } from "components/ui"
+import { useEffect, useState } from "react"
 import { apiGetRoles } from "services/RoleService"
 import useRequest from "utils/hooks/useRequest"
+import openNotification from "utils/openNotification"
+import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi"
+
+const { Tr, Th, Td, THead, TBody } = Table
 
 export default function RolesList() {
   const apiRequest = useRequest()
+  const [roles, setRoles] = useState([])
 
   useEffect(() => {
     const fetchRoles = async () => {
       const resp = await apiRequest(() => apiGetRoles())
-      console.log('resp:', resp)
+
+      if (resp.ok) {
+        setRoles(resp.data)
+      }
+
+      if (!resp.ok) {
+        openNotification('danger', 'Error', 'Error al obtener los roles')
+        console.error('Error:', resp.error)
+      }
     }
 
     fetchRoles()
   }, [apiRequest])
+
   return (
-    <div>
-      Role List
-    </div>
+    <Card>
+      <Table>
+        <THead>
+          <Tr>
+            <Th>#</Th>
+            <Th>Nombre</Th>
+            <Th>Nombre normalizado</Th>
+            <Th>Aplicación</Th>
+            <Th />
+          </Tr>
+        </THead>
+        <TBody>
+          {roles.map((role, index) => (
+            <Tr key={role.id}>
+              <Td>{index + 1}</Td>
+              <Td>{role.name}</Td>
+              <Td>{role.normalizedName}</Td>
+              <Td>{role.app}</Td>
+              <Td>
+                <div className="flex gap-2 min-w-max">
+                  <Tooltip title='Editar'>
+                    <Button size='sm' color='gray-600' icon={<HiOutlinePencilAlt />} variant="twoTone" />
+                  </Tooltip>
+                  <Tooltip title='Eliminar'>
+                    <Button size='sm' color='gray-600' icon={<HiOutlineTrash />} variant="twoTone" />
+                  </Tooltip>
+                </div>
+              </Td>
+            </Tr>
+          ))}
+        </TBody>
+      </Table>
+    </Card>
   )
 }
