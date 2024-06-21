753eb382d64a5c5c0577ffcf03c17e089e0a6c98
Anderson Frias <anderl.friaas@gmail.com>
2024-06-21 15:43:35 -0400

    [Add] URL de api produccion

--- src/configs/form.config/role.config.js
+++ src/configs/form.config/role.config.js
@@ -5,7 +5,7 @@ const validationSchema = Yup.object().shape({
     .min(3, 'El nombre debe tener al menos 3 caracteres')
     .required('El nombre es requerido'),
   normalizedName: Yup.string()
-    .matches(/^[a-z]+[a-z0-9\-.]*$/, 'El nombre normalizado solo puede contener letras minúsculas y números')
+    .matches(/^[a-z]+[a-z0-9\-_.]*$/, 'El nombre normalizado solo puede contener letras minúsculas y números')
     .min(3, 'El nombre normalizado debe tener al menos 3 caracteres')
     .required('El nombre normalizado es requerido'),
   app: Yup.string()
