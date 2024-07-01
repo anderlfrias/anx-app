6def2bdbdbbe2856b9066953c1039898f435993d
Anderson Frias <anderl.friaas@gmail.com>
2024-07-01 11:55:29 -0400

    Cambio para dezplegar en produccion

--- src/constants/api.constant.js
+++ src/constants/api.constant.js
@@ -1,5 +1,6 @@
 export const TOKEN_TYPE = 'Bearer '
 export const REQUEST_HEADER_AUTH_KEY = 'Authorization'
 // export const URL_API = 'http://172.20.6.46:3011/api'
-export const URL_API = 'http://172.20.1.63:3011/api'
+// export const URL_API = 'http://172.20.1.63:3011/api'
+export const URL_API = 'http://129.146.179.123:3011/api'
 // export const URL_API = 'https://xuser-api.cmsiglo21.app/api'
